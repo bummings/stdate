694f1fe5326eaeddb0f59b8e165f94f17c96c245
bummings <eabummings@gmail.com>
2019-03-13 06:43:32 -0400

    fixed node start script

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Nav from './components/Nav';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
+        <Nav />
         <header className='App-header'>
           <p>STDate</p>
         </header>
