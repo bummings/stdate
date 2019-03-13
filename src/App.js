56a81c2a43664e2257195ab49fdd50c8e7007d34
bummings <eabummings@gmail.com>
2019-03-13 07:54:25 -0400

    finalizes react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import './App.scss';
 import Nav from './components/Nav';
 import LandingPage from './components/LandingPage';
+import SignUp from './components/SignUp';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
+        <Route exact path='/' component={LandingPage} />
+
+        <Route path='/signup' component={SignUp} />
+
         <Nav />
         <LandingPage />
       </div>
