7ce4eac779bc9bb6ade8fa2d48c6e3a6df923e49
bummings <eabummings@gmail.com>
2019-03-13 23:23:07 -0400

    connects nodemon to backend

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,21 @@ server.get('/', (req, res) => {
   res.send('bruhhhhhhhh');
 });
 
+server.get('/users', (req, res) => {
+  const users = [
+    {
+      id: 1,
+      name: 'faux'
+    },
+    {
+      id: 2,
+      name: 'graf'
+    }
+  ];
+
+  res.status(200).json(users);
+});
+
 server.listen(5000, () => {
   console.log(`SERVER IS RUNNIN ON ${port}`);
 });
