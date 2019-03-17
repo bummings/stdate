6788a4fbe280004259f18044fa01ceb09a38d0e1
bummings <eabummings@gmail.com>
2019-03-16 22:39:43 -0400

    workin out all this front page grid, oy

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,11 +5,13 @@ class LandingPage extends Component {
   render() {
     return (
       <div className='landing'>
-        <p className='landing__headline'>
-          Join our community of 20,000 singles
-        </p>
-        <p className='landing__headline'>It's free & confidental</p>
-        <button className='button'>Get started</button>
+        <div className='landing__text'>
+          <p className='landing__headline'>
+            Join our community of 20,000 singles
+          </p>
+          <p className='landing__headline'>It's free & confidental</p>
+          <button className='button'>Get started</button>
+        </div>
 
         <LookingForForm />
       </div>
