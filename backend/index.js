19631fd5c0bb154e514974fe696814e2aa81624e
bummings <eabummings@gmail.com>
2019-03-18 23:09:53 -0400

    so i heard you like using knex to interface with a sqlite3 database

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+const db = knex(knexConfig.development);
+
 const server = express();
 const port = 5000;
 
@@ -19,6 +23,19 @@ server.get('/users', (req, res) => {
   ];
 
   res.status(200).json(users);
+});
+
+server.post('/users/', (req, res) => {
+  const user = req.body;
+
+  db.insert(user)
+    .into('users')
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 server.listen(5000, () => {
