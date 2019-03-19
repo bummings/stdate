0b55786791cecf5418929385ae9a89fdc78d7fc2
bummings <eabummings@gmail.com>
2019-03-18 23:29:29 -0400

    hm, do you remember how to use knex?

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const knex = require('knex');
-const knexConfig = require('./knexfile');
+const knexConfig = require('../knexfile');
 const db = knex(knexConfig.development);
 
 const server = express();
