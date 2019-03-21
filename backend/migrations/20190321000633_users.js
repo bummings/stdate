b4f388dc1aa0bf92bab2877de44bd921c5c3691a
bummings <eabummings@gmail.com>
2019-03-21 00:16:24 -0400

    schema is good for now, time to migrate

--- backend/migrations/20190321000633_users.js
+++ backend/migrations/20190321000633_users.js
@@ -1,8 +1,17 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users', user => {
+    user.increments('id');
+    user.string('name').notNullable();
+    user.string('email').unique();
+    user.string('password').notNullable();
+    user.integer('age').notNullable();
+    user.string('sex').notNullable();
+    user.string('city').notNullable();
+    user.integer('areaCode').notNullable();
+    user.string('bio');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('users');
 };
