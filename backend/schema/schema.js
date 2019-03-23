67b2eca092737208a3a3bf60e68cf6054c9497f8
bummings <eabummings@gmail.com>
2019-03-22 23:55:25 -0400

    adds comments

--- backend/schema/schema.js
+++ backend/schema/schema.js
@@ -1,7 +1,9 @@
 const graphql = require('graphql');
 
+// destructure the GraphQL types (string, integer, etc)
 const { GraphQLObjectType, GraphQLString } = graphql;
 
+// you'll define a type for each relationship
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
