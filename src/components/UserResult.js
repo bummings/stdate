5592b80a3923df907c25241514f27a63ca0d0418
bummings <eabummings@gmail.com>
2019-04-01 23:48:52 -0400

    hm

--- src/components/UserResult.js
+++ src/components/UserResult.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
+
+const id = 1;
 
 class UserResult extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      name: 'cuz'
+      user: [],
+      nickname: 'cuz'
     };
   }
 
+  componentDidMount() {
+    // const { id } = this.props.match.params;
+    Axios.get(`http://localhost:5000/users/1`)
+      .then(res => this.setState({ user: res.data.result }))
+      .then(console.log(this.state.user))
+      .catch(err => console.log(err));
+  }
+
   render() {
-    return <div>{this.state.name}</div>;
+    return (
+      <div>
+        <h1>{this.state.nickname}</h1>
+        <h4>{this.state.user}</h4>
+      </div>
+    );
   }
 }
 
